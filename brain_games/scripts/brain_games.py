88ff150d42b3aed21a3687a527a37eab524b5101
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-10 13:09:03 +0300

    edit script brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,3 +1,7 @@
-print("Welcome to the Brain Games!")
+#1~/root/usr/bin/python3
+def brain_games(greeting):
+        print(greeting)
+    
 if __name__ == '__main__':
-        brain_games()
+
+    brain_games("Welcome to the Brain Games!")
