31213554a3ec77b897e8cf92fcb0429d2367525c
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-10 14:45:37 +0300

    edit function brain_games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,5 @@
-#1~/root/usr/bin/python3
-def brain_games(greeting):
-        print(greeting)
-    
-if __name__ == '__main__':
+def brain_games():
+    print('Welcome to the Brain Games!')
+    if __name__ == '__main__':
+       brain_games()
 
-    brain_games("Welcome to the Brain Games!")
