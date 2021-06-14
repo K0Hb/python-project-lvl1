98d65697fca4c3da9026167a4a71852711145f53
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-14 17:18:32 +0300

    mass editing

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-import prompt
+
 from brain_games.cli import welcome_user
+
+
 def brain_games():
     print('Welcome to the Brain Games!')
     welcome_user()
- 
- 
+
+
 if __name__ == '__main__':
     brain_games()
