3b41e4de01b42f7f5787654ae62a1ac78043cb58
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-18 16:46:53 +0300

    add whitespace

--- brain_games/calculation_rnd.py
+++ brain_games/calculation_rnd.py
@@ -12,9 +12,9 @@ def generate_culc():
     while expression <= 3:
         number1 = random.randint(1, 50)
         number2 = random.randint(1, 50)
-        addition = '+'
-        subtraction = "-"
-        multiplication = '*'
+        addition = ' + '
+        subtraction = " - "
+        multiplication = ' * '
         rnd_oper = random.choice([addition, subtraction, multiplication])
         print('Question:' + str(number1) + rnd_oper + str(number2))
         result = eval_binary_expr(number1, rnd_oper, number2)
@@ -23,8 +23,8 @@ def generate_culc():
             expression += 1
             print('Correct!')
             if expression == 4:
-                print('Congratulations,' + name + '!')
+                print('Congratulations, ' + name + '!')
         elif int(answer) != result:
             print(str(answer) + ' is wrong answer ;(. Correct answer was' + str(result) + ".")
-            print("Let's try again," + name + '!')
+            print("Let's try again, " + name + '!')
             break
