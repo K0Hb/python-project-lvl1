5efa3f5d78b9533e794dff6c22e114b8c7816144
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-18 16:56:24 +0300

    Oh

--- brain_games/calculation_rnd.py
+++ brain_games/calculation_rnd.py
@@ -6,17 +6,17 @@ from brain_games.operator import eval_binary_expr
 
 def generate_culc():
     name = prompt.string('May I have your name? ')
-    print('Hello, ' + name + ' !')
+    print('Hello, ' + name + '!')
     print('What is the result of the expression?')
     expression = 1
     while expression <= 3:
         number1 = random.randint(1, 50)
         number2 = random.randint(1, 50)
-        addition = ' + '
-        subtraction = " - "
-        multiplication = ' * '
+        addition = '+'
+        subtraction = "-"
+        multiplication = '*'
         rnd_oper = random.choice([addition, subtraction, multiplication])
-        print('Question: ' + str(number1) + rnd_oper + str(number2))
+        print('Question: ' + str(number1) + ' ' + rnd_oper + ' ' + str(number2))
         result = eval_binary_expr(number1, rnd_oper, number2)
         answer = prompt.string('Your answer:')
         if int(answer) == result:
@@ -25,6 +25,6 @@ def generate_culc():
             if expression == 4:
                 print('Congratulations, ' + name + '!')
         elif int(answer) != result:
-            print(str(answer) + ' is wrong answer ;(. Correct answer was' + str(result) + ".")
+            print(str(answer) + ' is wrong answer ;(. Correct answer was ' + str(result) + ".")
             print("Let's try again, " + name + '!')
             break
