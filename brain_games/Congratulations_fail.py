3cd2e38498ee21f19c0f1100754346ccbd25febd
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-24 14:09:35 +0300

    correct from linter

--- brain_games/Congratulations_fail.py
+++ brain_games/Congratulations_fail.py
@@ -9,4 +9,3 @@ def Congratulations_fail(answer, correct_answer, name, expression):
     elif int(answer) != correct_answer:
         print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}.')
         print(f"Let's try again, {name}")
-    
