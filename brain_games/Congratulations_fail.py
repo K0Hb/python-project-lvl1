207fd259ab1575ee5101f2705a675dd003f49ada
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-24 16:45:05 +0300

    add '!'

--- brain_games/Congratulations_fail.py
+++ brain_games/Congratulations_fail.py
@@ -11,6 +11,6 @@ def Congratulations_fail(answer, correct_answer, name, expression):
         return expression
     elif int(answer) != correct_answer:
         print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}.')
-        print(f"Let's try again, {name}")
+        print(f"Let's try again, {name}!")
         expression += 3
         return expression
