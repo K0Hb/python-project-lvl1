c41b636ece1d512eb0f8f8d8412a6784de1afd6a
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-24 17:00:30 +0300

    delete import

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -2,7 +2,6 @@
 
 from brain_games.body_gcd import body_gcd
 from brain_games.What_is_your_name import welcome_user
-from brain_games.Congratulations_fail import Congratulations_fail
 
 
 def main():
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -2,7 +2,6 @@
 
 from brain_games.body_progression import body_progression
 from brain_games.What_is_your_name import welcome_user
-from brain_games.Congratulations_fail import Congratulations_fail
 
 
 def main():
