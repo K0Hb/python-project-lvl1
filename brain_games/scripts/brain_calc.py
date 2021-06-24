45890994daa2afd4c4754fa124ac737ae8ef9208
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-24 14:37:16 +0300

    add function Congratulations_fail

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,11 +2,13 @@
 
 from brain_games.calculation_rnd import generate_culc
 from brain_games.What_is_your_name import welcome_user
+from brain_games.Congratulations_fail import Congratulations_fail()
 
 
 def main():
     name = welcome_user()
     generate_culc(name)
+    Congratulations_fail()
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -2,11 +2,13 @@
 
 from brain_games.body_progression import body_progression
 from brain_games.What_is_your_name import welcome_user
+from brain_games.Congratulations_fail import Congratulations_fail()
 
 
 def main():
     name = welcome_user()
     body_progression(name)
+    Congratulations_fail()
 
 
 if __name__ == '__main__':
