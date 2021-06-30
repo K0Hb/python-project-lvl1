2e80a87fac4216c691a94350638dc2e3d0f6a082
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-06-30 18:09:29 +0300

    corect progression

--- brain_games/body_progression.py
+++ brain_games/body_progression.py
@@ -10,12 +10,11 @@ def body_progression(name, limit_round):
     Question = 1
     while Question <= limit_round:
         num1 = random.randint(1, 30)
-        num2 = random.randint(30, 60)
         rnd_num = random.randint(1, 5)
-        findex = random.randint(2, 10)
+        findex = random.randint(1, 9)
         correct_answer = num1 + (findex * rnd_num)
-        progression_line = filter_string(num1, num2, correct_answer, rnd_num)
-        print('Question:' + progression_line)
+        progression_line = filter_string(num1, correct_answer, rnd_num)
+        print(f'Question: {progression_line}')
         answer = prompt.string('Your answer:')
         Question = Congratulations_fail(answer, correct_answer,
                                         name, Question)
@@ -23,8 +22,8 @@ def body_progression(name, limit_round):
 
 def filter_string(num1, correct_answer, rnd_num):
     list = []
-    for i in range(num1, num1 + 10, rnd_num):
+    for i in range(num1, num1 + 10 * rnd_num, rnd_num):
         if i == correct_answer:
             i = '..'
-    list.append(i)
-    return list
+        list.append(str(i))
+    return (" ".join(list))
