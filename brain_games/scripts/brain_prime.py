6bf4c81127c38dedcc81336b7053d881b8dd633c
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-07-03 12:21:47 +0300

    Edit improt

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from brain_games.engine import play
-from brain_games.games import progression
+from brain_games.games import prime
 
 
 def main():
-    play(progression)
+    play(prime)
 
 
 if __name__ == '__main__':
