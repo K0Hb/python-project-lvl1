e12e79999ec5615658ef3685860083739457522e
goplit2010.konovalov@yandex.ru <goplit2010.konovalov@yandex.ru>
2021-07-19 18:39:24 +0300

    refactoring

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -17,7 +17,9 @@ def generate_question():
 
 
 def is_prime(number):
-    for number_enumeration in range(START_NUBMER, number + 1):
-        if number % number_enumeration == 0:
-            break
-    return number == number_enumeration
+    if number < 2:
+        return False
+    for divisor in range(2, (number // 2) + 1):
+        if number % divisor == 0:
+            return False
+    return True
